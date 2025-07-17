584db88cc48b25ccfeea7dd8f27f79b195f78ea4
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-17 14:42:22 +0530

    structure

---
+++ stuct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct x
+{
+	int age;
+	char name[10];
+	float sala;
+};
+void main()
+{
+struct x;
+x.age=2;
+x.name=sai;
+x.sala=6.5;
+printf("%d%s%f\n"x.age,x.name,x.sala);
+}
+
