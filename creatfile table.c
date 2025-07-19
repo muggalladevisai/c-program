3200709357f67bd3710edfbfdbdba5270b1682bb
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-19 10:52:21 +0530

    create a file table

---
+++ creatfile table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+/*	int n,i;
+FILE *ptr;
+printf("enter number");
+scanf("%d",&n);
+ ptr = fopen("D:\table.txt","w");
+ for(i=0;i<=10;i++)
+{
+	fprintf(ptr,"%d*%d=%d\n",n,i,n*i);
+}
+fclose(ptr);
+return 0; */
+}
