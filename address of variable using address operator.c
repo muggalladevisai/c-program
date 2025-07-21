5b6b7a76643f2421122ef6e034a21b0c76425125
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-21 14:45:40 +0530

    address of variable using address operator

---
+++ address of variable using address operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int x=5;
+	float salary=500;
+	char grade='a';
+	int *ptr=&x;
+	float *ptr1=&salary;
+	char *ptr2=&grade;
+	printf("%u\t%d\n",ptr*ptr);
+	printf("%u\t%f\n",ptr1*ptr1);
+	printf("%u\t%c\n",ptr2*ptr2);
+}
+	
