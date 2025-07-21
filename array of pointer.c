15ce2938f5130500c93f18c53d1d999785dd555f
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-21 14:45:10 +0530

    array of pointer.c

---
+++ array of pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int x[4]={10,20,30,40};
+	int *ptr[4];
+	int i;
+	for(i=0;i<=3;i++)
+	{
+		p[i] =&x[i];
+	}
+	printf("array elements are");
+	for(i=0;i<=3;i++)
+	{
+	printf("%d",*ptr[i]);
+}
+}
