dba13a1aa059175c82628ea2b103484a92bde960
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-08-04 15:52:59 +0530

    smallest array function.

---
+++ smallestarrayfunc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int findsmallest(int arr[],int n);
+void main()
+{
+	int arr[]={5,2,6,1,3};
+	for(i=0;i<n;i++)
+	{
+		printf("enter elements");
+		scanf("%d",&arr[i]);
+	}
+	int smallest=a[0]
+	for(i=1;i<n;i++)
+	{
+		if(arr[i]<smallest)
+		smallest=arr[i];
+	}
+	printf("%d",smallest);
+	return 0;
+}
